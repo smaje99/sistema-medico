1a6881219aa0da8178504a85cdc5e52a3c9c7d06
Sergio Majé <smajefranco@gmail.com>
2022-06-01 01:23:50 -0500

    :children_crossing: Add structure and styles of Public Navigation

--- frontend/src/components/Navigation/PublicNavigation/PublicNavigation.jsx
+++ frontend/src/components/Navigation/PublicNavigation/PublicNavigation.jsx
@@ -1,7 +1,46 @@
+import { NavLink } from 'react-router-dom';
+
+import routes from '@Helpers/routes';
+
+import './styles.css'
+
 const PublicNavigation = () => {
     return (
-        <div>PublicNavigation</div>
+        <nav className="navbar">
+            <NavLink to={routes.home} className="navbar__brand non-link">
+                Sistemas Médico
+            </NavLink>
+            <ul className="navbar__nav non-list">
+                <li className="navbar__nav--item">
+                    <NavLink
+                        to={routes.appointment}
+                        className="navbar__nav--link non-link"
+                    >
+                        Agendar cita
+                    </NavLink>
+                </li>
+                <li className="navbar__nav--item">
+                    <NavLink
+                        to={routes.consult}
+                        className="navbar__nav--link non-link"
+                    >
+                        Consultar cita
+                    </NavLink>
+                </li>
+                <li className="navbar__nav--item">
+                    <div className="vertical-separator"></div>
+                </li>
+                <li className="navbar__nav--item">
+                    <NavLink
+                        to={routes.login}
+                        className="navbar__nav--link non-link navbar__nav--login"
+                    >
+                        Iniciar sesión
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
-export default PublicNavigation
+export default PublicNavigation;
