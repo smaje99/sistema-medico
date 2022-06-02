4b3819e0846834cad73aa481c291befc55163229
Sergio Majé <smajefranco@gmail.com>
2022-06-02 00:31:38 -0500

    :construction: Set Toastify config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import AppRouter from "@Routers/AppRouter";
+import AppRouter from '@Routers/AppRouter';
 
 const App = () => (
+    <>
     <Router>
         <AppRouter />
     </Router>
+    <ToastContainer />
+    </>
 )
 
 export default App;
---
+++ frontend/src/helpers/config.js
@@ -0,0 +1,11 @@
+import { toast } from 'react-toastify';
+
+const config = {
+    toast: {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        className: 'toast',
+        autoClose: 8000
+    }
+}
+
+export default config;
