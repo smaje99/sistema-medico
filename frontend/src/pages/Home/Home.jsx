72ff6541359d8b304444b0154d2084440c15fbda
Sergio Majé <smajefranco@gmail.com>
2022-06-02 00:14:50 -0500

    :mag: Add Page title

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,31 +1,36 @@
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
 import routes from '@Helpers/routes'
 
 import './styles.css';
 
-const Home = () => {
-    return (
-        <main className="home">
-            <span className="home__title">
-                ¡Agenda tu cita médica con nosotros!
-            </span>
-            <div className="home__container">
-                <Link
-                    to={routes.appointment}
-                    className="home__link--primary non-link button-round"
-                >
-                    Agendar cita
-                </Link>
-                <Link
-                    to={routes.consult}
-                    className="home__link--secondary non-link button-round"
-                >
-                    Consultar cita
-                </Link>
-            </div>
-        </main>
-    )
-}
+const Home = () => (
+    <>
+    <Helmet>
+        <title>Inicio | Sistema Médico</title>
+    </Helmet>
+
+    <main className="home">
+        <span className="home__title">
+            ¡Agenda tu cita médica con nosotros!
+        </span>
+        <div className="home__container">
+            <Link
+                to={routes.appointment}
+                className="home__link--primary non-link button-round"
+            >
+                Agendar cita
+            </Link>
+            <Link
+                to={routes.consult}
+                className="home__link--secondary non-link button-round"
+            >
+                Consultar cita
+            </Link>
+        </div>
+    </main>
+    </>
+)
 
 export default Home;
