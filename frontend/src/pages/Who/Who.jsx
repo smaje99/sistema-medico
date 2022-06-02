5020359829b537e1c5df74aa3baba84c8c6355c9
Sergio Majé <smajefranco@gmail.com>
2022-06-01 23:44:23 -0500

    :sparkles: Who Page

---
+++ frontend/src/pages/Who/Who.jsx
@@ -0,0 +1,46 @@
+import './styles.css';
+
+const Who = () => (
+    <main className="who">
+        <div className="who__container">
+            <p className="who__text">
+                Eum dolor et stet dolor eirmod justo diam. Consequat eum blandit labore takimata
+                ut dolor veniam dolor ut at sed ad. Clita nonumy et voluptua illum amet dolores
+                sanctus gubergren tempor et clita vero nonumy. Ea diam sit stet aliquam accusam
+                liber consetetur ex elitr sit diam vel sadipscing ipsum nonumy te. Eos gubergren
+                eos. Clita sed takimata qui ut consequat lorem sit clita kasd. Nostrud velit sea
+                accumsan ut sed feugiat takimata amet augue labore erat labore dolores duo sanctus
+                ipsum vero dolore. Erat dolore aliquyam ea gubergren clita ipsum volutpat labore.
+                Ut aliquyam et nonumy voluptua facilisis facilisis labore justo nonumy. Diam sed
+                sanctus sea. At ipsum sit aliquyam sit sed erat elitr erat nulla. Mazim delenit
+                minim esse sea eos amet erat lorem nulla sanctus sed elitr sed ea.
+            </p>
+            <p className="who__text">
+                Dolore tation ipsum eos laoreet ea invidunt enim ut est exerci accumsan sit
+                aliquyam sit ad invidunt et. Accusam labore tempor rebum duis eum. Et et stet
+                sea consequat tempor suscipit invidunt ipsum invidunt tincidunt dolores.
+                Ea sed sanctus eos duo esse velit lorem tincidunt nonumy sadipscing lorem
+                sanctus erat dolores ut lorem erat elitr. Dolores diam amet aliquyam voluptua
+                invidunt vulputate. Rebum at rebum. Ipsum dolor dolor sed nonumy rebum amet eos
+                takimata soluta sanctus magna justo tempor feugiat ut takimata.
+                dignissim. Clita ad no stet.
+            </p>
+            <p className="who__text">
+                Vero justo accumsan rebum sea sadipscing erat duo magna tincidunt dolore voluptua.
+                Takimata minim sed kasd dolor. Dolor ipsum consetetur invidunt diam molestie justo
+                wisi invidunt ex dolore autem duis ipsum dolor dolor ea. In invidunt clita duis
+                facilisis iusto delenit kasd sit nihil.
+            </p>
+            <p className="who__text">
+                Dolore labore est at lorem gubergren erat et consequat sed velit no.
+                Dolore magna aliquyam kasd vero esse gubergren nihil amet suscipit diam sea.
+                Et vero sed dolores lobortis in accusam diam erat dolores quod eos ipsum sed duo et.
+                Augue eum esse amet sed no sit at consetetur vero dolor kasd voluptua et dolor
+                ipsum tempor et. Veniam magna mazim et labore esse. Rebum suscipit elitr at eum
+                et enim ipsum sit laoreet dignissim dolores rebum dolor.
+            </p>
+        </div>
+    </main>
+)
+
+export default Who
--- frontend/src/routers/AppRouter.jsx
+++ frontend/src/routers/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { PrivateLayout, PublicLayout } from '@Components/Layout';
 
 import Home from '@Pages/Home';
+import Who from '@Pages/Who';
 
 import React from 'react'
 
@@ -11,6 +12,7 @@ const AppRouter = () => (
         <Route path="/">
             <Route element={<PublicLayout />}>
                 <Route index element={<Home />} />
+                <Route path="who" element={<Who />} />
 
                 <Route path="*" element={<h1>Página no encontrada</h1>} />
             </Route>
