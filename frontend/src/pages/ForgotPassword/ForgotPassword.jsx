46f35fb4433bb854238f2393cee5b2ead41e5015
Sergio Majé <smajefranco@gmail.com>
2022-06-11 23:07:19 -0500

    :construction: Add Forgot Password form component and styles

--- frontend/src/pages/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,6 +1,9 @@
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom'
 
 import ForgotPasswordForm from './ForgotPasswordForm';
+
+import routes from '@Helpers/routes';
 
 import './styles.css';
 
@@ -10,12 +13,17 @@ const ForgotPassword = () => {
         <Helmet>
             <title>¿Olvidaste tu contraseña? | Sistema Médico</title>
         </Helmet>
-        <section className="forgot-password">
-            <span className="forgot-password__title">
-                ¿Olvidaste tu contraseña?
-            </span>
-            <ForgotPasswordForm />
-        </section>
+        <main className="forgot-password">
+            <section className="forgot-password__container">
+                <span className="forgot-password__title">
+                    ¿Olvidaste tu contraseña?
+                </span>
+                <ForgotPasswordForm />
+                <Link to={routes.login} className="forgot-password__back non-link">
+                    Volver al inicio de sesión
+                </Link>
+            </section>
+        </main>
         </>
     )
 }
--- frontend/src/pages/ForgotPassword/ForgotPasswordForm.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPasswordForm.jsx
@@ -1,6 +1,40 @@
+import { useForm } from 'react-hook-form';
+import { FaUser  } from 'react-icons/fa';
+
 const ForgotPasswordForm = () => {
+    const {
+        reset,
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const handleForgotPassword = (formData) => {}
+
     return (
-        <div>ForgotPasswordForm</div>
+        <form
+            className="forgot-password__form"
+            onSubmit={handleSubmit(handleForgotPassword)}
+        >
+            <label
+                htmlFor="forgot-password-username"
+                className="forgot-password__form__content"
+            >
+                <FaUser />
+                <input
+                    type="text"
+                    id="forgot-password-username"
+                    className="forgot-password__form--input"
+                    placeholder="Ingrese su usuario"
+                    { ...register('username') }
+                />
+            </label>
+            <input
+                type="submit"
+                className="forgot-password__form--submit button-round"
+                value="Recuperar contraseña"
+            />
+        </form>
     )
 }
 
