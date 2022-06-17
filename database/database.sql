0806461ad4cb960bb0765ee1c69aa70223ccbbcf
Sergio Majé <smajefranco@gmail.com>
2022-06-16 20:09:00 -0500

    :construction: Create Person table in database

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,18 @@
 create database if not exists sistema_medico;
+
+create table if not exists sistema_medico.Person (
+    dni int unsigned not null,
+    `name` text not null,
+    surname text not null,
+    `address` text,
+    email text not null,
+    phone int unsigned not null,
+    gender enum('M', 'F', 'T') not null,
+    birthdate datetime not null,
+    document_type enum('R.C.', 'T.I.', 'C.C.', 'C.E.') not null,
+    blood_type enum('AB+', 'AB-', 'A+', 'A-', 'B+', 'B-', 'O+', 'O-'),
+    created_at timestamp not null default now(),
+
+    constraint pk_person primary key (dni),
+    constraint chk_person_phone check(length(convert(phone, char)) = 10)
+);
