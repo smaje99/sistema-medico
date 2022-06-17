c3d77d8c4a1d1606ff4191d1e1b0349555968c76
Sergio Majé <smajefranco@gmail.com>
2022-06-16 22:48:39 -0500

    :seedling: Create Aplication root

---
+++ backend/src/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title='Sistema Médico API',
+    description='Gestión de las operaciones CRUD y lógica de negocio del Sistema Médico',
+    version='0.0.1'
+)
